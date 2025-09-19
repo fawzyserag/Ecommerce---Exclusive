8c97de2a4de045168f28e1f11c229ca1b1010245
fawzyserag <fawzyserag123@gmail.com>
2025-09-19 11:39:26 +0300

    Make Tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: "#db4444",
+        light: "#f0f3f2",
+        rating: "#ffc908"
+      },
+      fontFamily: {
+        main: ["Encode Sans Expanded", "sans-serif"]
+      },
+      boxShadow: {
+        main: "rgba(145, 158, 171, 0.2) 0px 2px 4px -1px, rgba(145, 158, 171, 0.14) 0px 4px 5px 0px, rgba(145, 158, 171, 0.12) 0px 1px 10px 0px"
+      }
+    }
+  },
+  plugins: [
+    require("flowbite/plugin")
+  ],
+}
